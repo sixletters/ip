7e01f3afeb959c47e1e8963545710cc5d4c69000
harris maung <harrismaung3@gmail.com>
2020-08-25 13:52:28 +0800

    Update Duke.java
    Level-1, Echo,greet,exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" +
                 " What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        int exit = 0;
+        while(exit == 0) {
+            String command = sc.next();
+            if (command.equals("bye")) {
+                exit = 1;
+                System.out.println("Bye. Hope to see you again soon!");
+            }
+            else{
+                System.out.println(command);
+            }
+        }
 
     }
 }
